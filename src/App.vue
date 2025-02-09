045c0a19b10c3729a19601a46cedd5490f573fc8
Jeremyah Botello <jbotel17@stu.southtexascollege.edu>
2025-02-09 01:36:11 -0600

    first comit all the files

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue";
+
+const contacts = ref([]);
+    const name = ref("");
+    const phone = ref("")
+
+
+</script>
+
+<template>
+<div class="container">
+  <h1>Contact List</h1>
+
+  <div class="form-group">
+    <label for="name">Name:</label>
+    <input v-model="name" type="text" id="name" placeholder="Enter Name">
+
+    <label for="phone">Phone Number:</label>
+    <input v-model="phone" type="text" id="phone" placeholder="Enter Phone Number">
+
+    <button @click="addContact">Add Contact</button>
+  </div>  
+
+</div>
+
+
+</template>
